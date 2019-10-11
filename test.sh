796f02d3b620da2a8de9415b743182cec9f5d95f
kyle11235 <aizuguo_@126.com>
2019-10-11 15:41:21 +0800

    test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-curl -H "Content-Type: application/json" -d @./spec/sample.req.json localhost:3000/components/check
+curl -H "Content-Type: application/json" -d @./spec/sample.req.json localhost:30001/components/check
 
 // curl -X GET localhost:3000/components
